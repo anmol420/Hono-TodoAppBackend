145efc9fe17787281aac4f817d829c06bd9062e6
anmol420 <anmolanand2k15@gmail.com>
2024-12-30 06:11:58 +0530

    Added Minor Bug Fix

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,21 @@ import { connectDB } from "./db/index.db";
 
 const app = new Hono();
 
-connectDB();
-
-app.notFound((c) => c.html('<h1>404-Not Found</h1>'))
+app.notFound((c) => c.html('<h1>404-Not Found</h1>'));
 
 // ROUTES
 import healthRouter from "./routes/healthCheck.routes";
 app.route('/api/v1', healthRouter);
 
-export default {
-  port: 8080,
-  fetch: app.fetch,
-};
+connectDB()
+  .then(() => {
+    Bun.serve({
+      fetch: app.fetch,
+      port: process.env.PORT 
+    });
+    console.log(`App Started On Port - ${process.env.PORT}`);
+  })
+  .catch((e: unknown) => {
+    const message = e instanceof Error ? e.message : "Database Connection Error.";
+    console.log(`Error - ${message}`);
+  });
