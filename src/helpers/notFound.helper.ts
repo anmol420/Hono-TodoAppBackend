d57be2a9f0143f18fe48bf308270be53acacf1ab
anmol420 <anmolanand2k15@gmail.com>
2024-12-30 06:47:26 +0530

    Added Cors Setup & Not Found Page

---
+++ src/helpers/notFound.helper.ts
@@ -0,0 +1,16 @@
+const NotFoundTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NOT FOUND</title>
+</head>
+<body style="text-align: center; background-color: black; color: whitesmoke;">
+    <h1>404 - Not Found</h1>
+    <h3>Page Does Not Exists, Kindly Contact The Admin.</h3>
+</body>
+</html>
+`;
+
+export default NotFoundTemplate;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+
 import { connectDB } from "./db/index.db";
+import NotFoundTemplate from "./helpers/notFound.helper";
 
 const app = new Hono();
 
-app.notFound((c) => c.html('<h1>404-Not Found</h1>'));
+app.use(cors({
+  origin: `${Bun.env.CORS_ORIGIN}`,
+  credentials: true,
+}));
+
+app.notFound((c) => c.html(NotFoundTemplate));
 
 // ROUTES
 import healthRouter from "./routes/healthCheck.routes";
@@ -13,9 +21,9 @@ connectDB()
   .then(() => {
     Bun.serve({
       fetch: app.fetch,
-      port: process.env.PORT 
+      port: Bun.env.PORT 
     });
-    console.log(`App Started On Port - ${process.env.PORT}`);
+    console.log(`App Started On Port - ${Bun.env.PORT}`);
   })
   .catch((e: unknown) => {
     const message = e instanceof Error ? e.message : "Database Connection Error.";
