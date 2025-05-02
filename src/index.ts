3a7b165ffb302fa34f735b1063a5f0210bab6d8c
anmol420 <anmolanand2k15@gmail.com>
2025-05-03 03:40:34 +0530

    Added Minor Fix + RabbitMQ Update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import app from "./app";
-import aj from "./libs/arcjet";
+import aj from "./libs/arcjet.lib";
 
-import { connectDB } from "./db/index.db";
+import {connectDB} from "./db/index.db";
 import errorMessage from "./helpers/errorMessage.helper";
+import startConsumer from "./utils/mq/consumers";
 
 connectDB()
-  .then(() => {
-    Bun.serve({
-      fetch: aj.handler(app.fetch),
-      port: process.env.PORT 
+    .then(() => {
+        startConsumer()
+            .then(() => {
+                console.log("Starting consumer...");
+            }).catch((err: Error) => {
+            console.log(`Error starting consumer: ${errorMessage(err)}`);
+        });
+        Bun.serve({
+            fetch: aj.handler(app.fetch),
+            port: process.env.PORT
+        });
+        console.log(`App Started On Port - ${process.env.PORT}`);
+    })
+    .catch((e: Error) => {
+        console.log(`Error - ${errorMessage(e)}`);
     });
-    console.log(`App Started On Port - ${process.env.PORT}`);
-  })
-  .catch((e: unknown) => {
-    console.log(`Error - ${errorMessage(e)}`);
-  });
